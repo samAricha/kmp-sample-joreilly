75e9169f3bc9ac2ea2dc1277f5b3759f96575f38
Aricha Samson <59553199+samAricha@users.noreply.github.com>
2024-01-23 15:06:39 +0300

    Initialize renovate/io.ktor-ktor-server-cors-2.x

---
+++ common/build.gradle.kts
@@ -0,0 +1,111 @@
+@file:Suppress("OPT_IN_USAGE")
+
+plugins {
+    kotlin("multiplatform")
+    id("kotlinx-serialization")
+    id("com.android.library")
+    id("app.cash.sqldelight")
+    id("com.google.devtools.ksp")
+    id("com.rickclephas.kmp.nativecoroutines")
+    id("io.github.luca992.multiplatform-swiftpackage") version "2.2.1"
+}
+
+android {
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    namespace = "com.surrus.common"
+}
+
+kotlin {
+    jvmToolchain(17)
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64(),
+        watchosArm64(),
+        watchosSimulatorArm64(),
+        macosArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "common"
+        }
+    }
+
+    androidTarget()
+    jvm()
+
+    js(IR) {
+        useCommonJs()
+        browser()
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.bundles.ktor.common)
+            implementation(libs.kotlinx.coroutines)
+            implementation(libs.kotlinx.serialization)
+
+            implementation(libs.sqldelight.runtime)
+            implementation(libs.sqldelight.coroutines.extensions)
+
+            api(libs.koin.core)
+            implementation(libs.koin.test)
+
+            api(libs.kermit)
+        }
+
+        commonTest.dependencies {
+            implementation(libs.koin.test)
+            implementation(libs.kotlinx.coroutines.test)
+            implementation(kotlin("test-common"))
+            implementation(kotlin("test-annotations-common"))
+        }
+
+        androidMain.dependencies {
+            implementation(libs.ktor.client.android)
+            implementation(libs.sqldelight.android.driver)
+        }
+
+        jvmMain.dependencies {
+            implementation(libs.ktor.client.java)
+            implementation(libs.sqldelight.sqlite.driver)
+            implementation(libs.slf4j)
+        }
+
+        appleMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+            implementation(libs.sqldelight.native.driver)
+        }
+
+        jsMain.dependencies {
+            implementation(libs.ktor.client.js)
+        }
+    }
+}
+
+sqldelight {
+    databases {
+        create("PeopleInSpaceDatabase") {
+            packageName.set("com.surrus.peopleinspace.db")
+        }
+    }
+}
+
+multiplatformSwiftPackage {
+    packageName("PeopleInSpaceKit")
+    swiftToolsVersion("5.9")
+    targetPlatforms {
+        iOS { v("14") }
+        macOS { v("12")}
+    }
+}
+
+kotlin.sourceSets.all {
+    languageSettings.optIn("kotlin.experimental.ExperimentalObjCName")
+}
+
---
+++ wearApp/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+android {
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.surrus.peopleinspace"
+        minSdk = libs.versions.minWearSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get()
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            // temporary hack for local testing of release builds
+            signingConfig = signingConfigs.getByName("debug")
+        }
+        create("benchmark") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isDebuggable = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "benchmark-rules.pro")
+            // temporary hack for local testing of release builds
+            signingConfig = signingConfigs.getByName("debug")
+            matchingFallbacks.addAll(listOf("release", "debug"))
+        }
+    }
+
+    namespace = "com.surrus.peopleinspace"
+}
+
+dependencies {
+    implementation(libs.osmdroidAndroid)
+
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.metrics)
+
+    implementation(libs.wear.compose.founndation)
+    implementation(libs.wear.compose.material)
+    implementation(libs.wear.compose.navigation)
+
+    implementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.horologist.compose.layout)
+    implementation(libs.glance.tiles)
+    implementation(libs.coilCompose)
+
+    implementation(libs.koin.core)
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.compose)
+
+    implementation(libs.okhttp)
+    implementation(libs.loggingInterceptor)
+
+    testImplementation(libs.robolectric)
+    testImplementation(libs.junit)
+
+    // Compose testing dependencies
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    debugImplementation(libs.androidx.tracing)
+
+    implementation(project(":common"))
+}
