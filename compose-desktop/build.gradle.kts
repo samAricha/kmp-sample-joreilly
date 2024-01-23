579343bbf61ece513a58abe319f68150fa1aa559
Aricha Samson <59553199+samAricha@users.noreply.github.com>
2024-01-23 15:06:34 +0300

    Initialize renovate/io.ktor-ktor-serialization-kotlinx-json-2.x

---
+++ compose-desktop/build.gradle.kts
@@ -0,0 +1,20 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose") version libs.versions.composeMultiplatform
+    application
+}
+
+group = "me.joreilly"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation(libs.imageLoader)
+    implementation(project(":common"))
+}
+
+application {
+    mainClass.set("MainKt")
+}
